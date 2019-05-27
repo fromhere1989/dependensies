f287ba593f69c48c7ef3fb7f7f74156917eccbdc
fromhere1989 <fromhere1989@gmail.com>
2019-05-27 20:04:34 +0300

    added dep. and config. for parcel and styled comp.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component} from "react";
-import "./App.css";
+import React, { Component} from "react"
+import styled from 'styled-components'
+
+const Greeting = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-around;
+width: 200px;
+height: auto;
+background-color: rgba(120, 140, 188, 0.4);
+color: rgba(55, 195, 88, 3);
+`
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
+      <Greeting>
         <h1> Hello, World! </h1>
-      </div>
+        <h2> I`m glad to see you! </h2>
+      </Greeting>
     );
   }
 }
